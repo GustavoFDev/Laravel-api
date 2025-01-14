351d971f699d98ae6ffb9def49556014f60299ae
FsaraiRod <143908353+FsaraiRod@users.noreply.github.com>
2025-01-14 14:17:56 -0600

    Ruta Creencias personales

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\ApplicantController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CreenciasPController;
 use App\Http\Controllers\PruebaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
@@ -12,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('pruebas', PruebaController::class)->middleware('auth:sanctum');
 
+Route::apiResource(name: 'creencias_personales1', controller: CreenciasPController::class);
 
 Route::apiResource('applicant', ApplicantController::class);
+
+Route::apiResource('test_view', ApplicantController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 
